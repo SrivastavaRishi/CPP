73743e3dc787d027ed8a057991ee53d63e84d0c2
Rishi <srishi0310@gmail.com>
2022-08-25 19:10:31 +0530

    Temporary Files

---
+++ A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+ 
+#define deb(x) cout<<#x<<": "<<x<<"\n";
+#define deb2(x, y) cout<<#x<<" "<<x<<" "<<#y<<" "<<y<<"\n";
+ 
+void init_code() {
+	fast_io;
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+	cout<<"Hello World !!!"<<endl;
+}
+ 
+ 
+signed main() {
+	init_code();
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+}
